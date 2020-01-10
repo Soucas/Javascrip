0256538a7c26c75357ff7b47990ad9812548dca9
Soucas <56599189+Soucas@users.noreply.github.com>
2020-01-10 12:31:53 +0100

    Add files via upload

---
+++ tarefa3_mates/escripita.js
@@ -0,0 +1,33 @@
+  
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: 
+*/
+var pescado;
+pescado = 10;
+alert(pescado);
+
+var pescado = 22.2222;
+var carne = 33.3333;
+var total = pescado + carne;
+alert(total);
+
+var caderno = 8.4444;
+var raiz = Math.pow(caderno , 2);
+alert(raiz);
+
+var patata = 4.2222;
+var cuadrado = Math.sqrt(patata);
+alert(cuadrado) ;
+
+var pi = Math.PI;
+var paco = Math.round(pi);
+alert(paco);
+
+var circulo = Math.random(0 ,3);
+alert(alert);
+
+var ciervo =10.45454, conejo =4.1566 , papel= 6.15145;
+var pajaro = Math.min (conejo, ciervo, papel);
+alert(pajaro);
