16de53d56e7a873b28626893727da0680fd71a23
Soucas <56599189+Soucas@users.noreply.github.com>
2020-01-14 18:10:41 +0100

    Add files via upload

---
+++ tarefa4_tareas/escripita.js
@@ -0,0 +1,21 @@
+  
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripito.js
+	Usado: Tarefa 4
+*/
+
+// var euSon = "Profe" + " que " + " conta " + " JavaScript ";
+// alert(euSon);
+
+// var oficio = "Profe";
+// var longura = oficio.length;
+// alert(longura);
+
+var oficio = "Profe";
+var rebanada = oficio.slice(3,5);
+alert(rebanada);
+
+var mensaje = "Hola";
+var posicion = mensaje.indexOf('a'); 
+alert(posicion);
---
+++ tarefa5_animados/escripita.js
@@ -0,0 +1,25 @@
+  
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripita.js
+	Usado: Tarefa 5
+*/
+/*
+var nome;
+nome = prompt("Como te chamas?   ");
+alert("Guau... que nome máis interesante " + nome);
+alert("Ti es unha grande persoa " + nome);
+alert("Vales moito " + nome);
+alert("Non hai ninguén máis elegante ca " + nome);
+*/
+
+var ducias;
+ducias = prompt("cantas ducias de ovos leva a tortilla ");
+
+var numDucias;
+numDucias = parseInt(ducias);
+alert(numDucias);
+
+var ovos;
+ovos = 12* numDucias;
+alert(ovos)
