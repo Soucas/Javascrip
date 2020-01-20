30baf960348c2654b6dbb499adc9d9ce3ee2dbc0
Soucas <56599189+Soucas@users.noreply.github.com>
2020-01-20 16:10:49 +0100

    Add files via upload

---
+++ tarefa8_medallista/escripita.js
@@ -0,0 +1,19 @@
+  
+/* 
+	Código en JavaScript
+	Nome do ficheiro: escripita.js
+	Usado:
+
+*/
+
+var posicion = 332121;
+if(posicion == 1) {
+	alert("Medalla de ouro");
+	alert("Buena")
+} else if (posicion == 2) {
+	alert("Medalla de plata");
+}else if (posicion == 3) {
+	alert("Medalla de bronce");
+}else {
+	alert("Pringado");
+}
